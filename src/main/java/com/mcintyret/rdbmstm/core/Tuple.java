82395999c103043091710aa38ab7c99dbcdcfd48
Tom McIntyre <tom.mcintyre5@gmail.com>
2014-08-16 20:37:23 +0100

    Improved core type structure

--- src/main/java/com/mcintyret/rdbmstm/core/Tuple.java
+++ src/main/java/com/mcintyret/rdbmstm/core/Tuple.java
@@ -1,8 +1,5 @@
 package com.mcintyret.rdbmstm.core;
 
-import java.util.Iterator;
-import java.util.Map;
-
 public interface Tuple extends Iterable<Value>, Columnar {
 
     Value select(String colName);
